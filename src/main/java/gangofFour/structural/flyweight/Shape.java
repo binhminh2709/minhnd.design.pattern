6af9c07e0f30dd4f3843bbb069297a310f1e6bd8
binhminh <nguyendinhminh2709@gmail.com>
2016-11-14 01:44:38 +0700

    strategy

--- src/main/java/gangofFour/structural/flyweight/Shape.java
+++ src/main/java/gangofFour/structural/flyweight/Shape.java
@@ -1,7 +1,10 @@
 package gangofFour.structural.flyweight;
 
+import java.awt.*;
+
 /**
  * Created by nguyen on 10/7/2016.
  */
 public interface Shape {
+    public void draw(Graphics g, int x, int y, int width, int height, Color color);
 }
